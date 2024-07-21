c72a72b6d2eea84d927a240a7dac29c79ddfcd0f
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-21 16:17:13 +0100

    1-simple_pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" indexing page and the page size """
+import csv
+import math
+from typing import Tuple, List
+
+
+class Server:
+    """
+    Server class to paginate a database of popular names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for rows in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        pass
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ indexing page """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
