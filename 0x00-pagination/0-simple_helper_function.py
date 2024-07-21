644d024aa5008ad9a83ae49777a3d83d92cc97b4
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-21 10:51:02 +0100

    helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ indexing page """
     start = (page - 1) * page_size
-    end = page * page_size 
+    end = page * page_size
     return (start, end)
