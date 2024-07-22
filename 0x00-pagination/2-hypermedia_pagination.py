b85d2c9eb9c4cb53f3b78bce596825fc0fe55fb6
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-22 11:48:02 +0100

    media_pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" indexing page and the page size """
+import unittest
+import csv
+import math
+from typing import Tuple, List
+
+
+class Server:
+    """
+    Server class to paginate a database of popular names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for rows in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ ensuring the dataset returns an integer """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.dataset()
+        try:
+            start, end = index_range(page, page_size)
+            # if start >= len(dataset):
+            return []
+        except IndexError:
+            return dataset[start:end]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ indexing page """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+def get_hyper(page: int, page_size: int) -> Tuple[int, int]:
+    """ 
