e7496fd1d93e1c7794596788317bdfc42e0c35bd
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-23 04:17:26 +0100

    hypermedia_pag

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
-""" indexing page and the page size """
-import unittest
+"""
+Hypermedia pagination
+"""
 import csv
 import math
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Any
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start
+     index and an end index corresponding to the
+     range of indexes to return in a list for
+     those particular pagination parameters
+    :param page:
+    :param page_size:
+    :return:
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
 
 
 class Server:
-    """
-    Server class to paginate a database of popular names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -16,36 +31,47 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
-                dataset = [row for rows in reader]
+                dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ ensuring the dataset returns an integer """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        dataset = self.dataset()
-        try:
-            start, end = index_range(page, page_size)
-            # if start >= len(dataset):
+        """
+        Find the correct indexes to paginate dataset
+        :param page:
+        :param page_size:
+        :return:
+        """
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
             return []
-        except IndexError:
-            return dataset[start:end]
+        return self.dataset()[start:end]
 
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ indexing page """
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
-
-
-def get_hyper(page: int, page_size: int) -> Tuple[int, int]:
-    """ 
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """
+        Return dataset as a dictionary
+        :param page:
+        :param page_size:
+        :return:
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
