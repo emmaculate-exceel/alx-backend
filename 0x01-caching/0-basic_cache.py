9e2bb4d30aa3c8b43f15968ff35e82c837746a01
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-25 04:58:35 +0100

    basic_cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" a class for the caching system """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic cache """
+    def __init__(self):
+        """ initializing the object """
+        super().__init__()
+        self.cache_data[key] = item
+
+    def put(self, key, item):
+        """ put method """
+        if key is not None or item is not None:
+            value = self.cache_data.get(key)
+            return value
+
+    def get(self, key):
+        """ get method """
+        if key is None or key is not in self.cache_data.get(item):
+            return None
