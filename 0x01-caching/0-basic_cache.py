62020a7b9db5e39c0f049f3271aa9511b29f0dcb
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-25 12:15:42 +0100

    basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,19 +8,19 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """ Basic cache """
-    
+
     def __init__(self):
-        super().__init__()
         self.cache_data = {}
 
     def put(self, key, item):
         """ put method """
         if key is not None or item is not None:
-            return self.cache_data[key] = item
+            self.cache_data[key] = item
         else:
             pass
 
     def get(self, key):
         """ get method """
-        if key is None or key is not in self.cache_data.get(item):
+        if key is None:
             return None
+        return self.cache_data.get(key)
