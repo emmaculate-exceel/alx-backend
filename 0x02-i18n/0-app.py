c0af0268d2fd1e8efad085d791666dfa3eb496c6
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-30 18:06:13 +0100

    update app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+# A flask app for a single route
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
