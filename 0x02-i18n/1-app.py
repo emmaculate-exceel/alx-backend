0fa9bc2c1687bea2f5e8db3381f40378ec55cb3e
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-30 19:15:18 +0100

    1-app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+""" babel object """
+from flask import request, render_template
+from flask_babel import Babel
+
+
+app = Flask(name)
+babel = Babel(app, locale_selector=get_locale)
+
+
+class Config:
+    """ selecting language for babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'UTC'
+
+
+def get_locale():
+    """ select language """
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES'])
+
+
+app.config.from_object(Config)
