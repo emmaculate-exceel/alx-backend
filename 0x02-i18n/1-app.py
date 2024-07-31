b0427d514e6b65251c50375999db84c633a487db
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-31 02:54:53 +0100

    1-app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
-""" babel object """
-from flask import request, render_template
-from flask_babel import Babel
+"""
+Basic Babel setup
+"""
+
 import babel
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config:
-    """ selecting language for babel """
-    LANGUAGES = ["en", "fr"]
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
@@ -26,5 +31,5 @@ def index():
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
